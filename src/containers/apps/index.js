2542d54d42f03193c31acc8932e58d26eb324dcb
Eric Muyser <eric@muyser.com>
2018-03-13 20:22:40 -0700

    Mocking out apps screen

--- src/containers/apps/index.js
+++ src/containers/apps/index.js
@@ -2,24 +2,65 @@ import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Layout, Menu, Table, Badge, Breadcrumb, Dropdown, Icon, Spin } from 'antd'
+import { Layout, Menu, Table, Badge, Breadcrumb, Dropdown, Icon, Spin, Tabs } from 'antd'
 import { WhiteSpace } from 'antd-mobile'
 
 import AdvancedLayout from '../../components/advanced-layout'
 
 const { SubMenu } = Menu
 const { Header, Content, Sider } = Layout
+const TabPane = Tabs.TabPane
 
 
-
-const Container = ({ marketplace }) => (
+const Container = ({ republic, marketplace }) => (
     <AdvancedLayout>
         <Content style={{ 'padding': '10px', 'marginTop': '10px' }}>
-            {marketplace.apps.map((app) => (
+            <h2>My Apps <button>Create App</button></h2>
+            {republic.currentCitizen.apps.map((app) => (
                 <div>
                     <Link to={"app/" + app.id}>{app.name}</Link>
                 </div>
             ))}
+            filters
+            featured
+            special offers
+
+            <Tabs defaultActiveKey="new-trending" size="small">
+                <TabPane tab="New and Trending" key="new-trending">
+                    <br />
+                    {marketplace.apps.newTrending.map((app) => (
+                        <div>
+                            <Link to={"app/" + app.id}>{app.name}</Link>
+                        </div>
+                    ))}
+                </TabPane>
+                
+                <TabPane tab="Top Sellers" key="top-sellers">
+                    <br />
+                    {marketplace.apps.topSellers.map((app) => (
+                        <div>
+                            <Link to={"app/" + app.id}>{app.name}</Link>
+                        </div>
+                    ))}
+                </TabPane>
+                
+                <TabPane tab="Upcoming" key="upcoming">
+                    <br />
+                    {marketplace.apps.upcoming.map((app) => (
+                        <div>
+                            <Link to={"app/" + app.id}>{app.name}</Link>
+                        </div>
+                    ))}
+                </TabPane>
+                
+                <TabPane tab="Specials" key="specials">
+                    {marketplace.apps.specials.map((app) => (
+                        <div>
+                            <Link to={"app/" + app.id}>{app.name}</Link>
+                        </div>
+                    ))}
+                </TabPane>
+            </Tabs>
         </Content>
     </AdvancedLayout>
 )
@@ -27,7 +68,8 @@ const Container = ({ marketplace }) => (
 Container.displayName = 'apps/Container'
 
 const mapStateToProps = state => ({
-    marketplace: state.marketplace
+    marketplace: state.marketplace,
+    republic: state.republic
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
