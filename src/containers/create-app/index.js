087db55c5edcdf914c16ba140025e7281df3e3e0
Eric Muyser <eric@muyser.com>
2018-04-04 13:48:20 -0700

    Clean up create app screen

--- src/containers/create-app/index.js
+++ src/containers/create-app/index.js
@@ -14,7 +14,6 @@ const { Header, Content, Sider } = Layout
 const FormItem = Form.Item
 const Option = Select.Option
 
-// name, version, category, files, checksum, permissions
 const formItemLayout = {
     labelCol: { span: 4 },
     wrapperCol: { span: 14 },
@@ -23,6 +22,88 @@ const formItemLayout = {
 const buttonItemLayout = {
     wrapperCol: { span: 14, offset: 4 },
 }
+
+const MainContent = ({ app, account, form: { getFieldDecorator }, handleSubmit}) => (
+    <AdvancedLayout>
+        <Content style={{ 'padding': '10px', 'marginTop': '10px', 'background': '#fff' }}>
+            Create App
+                    <br />
+            <Row>
+                <Col xs={24} sm={24} md={16} lg={16} xl={10}>
+                    <Form layout="horizontal" onSubmit={handleSubmit}>
+                        <FormItem
+                            label="Name"
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('name', {
+                                rules: [{ required: true, message: 'Please input a name' }],
+                            })(
+                                <Input />
+                                )}
+                        </FormItem>
+                        <FormItem
+                            label="Version"
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('version', {
+                                rules: [{ required: true, message: 'Please input a version' }],
+                            })(
+                                <Input />
+                                )}
+                        </FormItem>
+                        <FormItem
+                            label="Category"
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('category', {
+                                rules: [{ required: true, message: 'Please input a category' }],
+                            })(
+                                <Input />
+                                )}
+                        </FormItem>
+                        <FormItem
+                            label="Files"
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('files', {
+                                rules: [{ required: true, message: 'Please input a files' }],
+                            })(
+                                <Input />
+                                )}
+                        </FormItem>
+                        <FormItem
+                            label="Checksum"
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('checksum', {
+                                rules: [{ required: true, message: 'Please input a checksum' }],
+                            })(
+                                <Input />
+                                )}
+                        </FormItem>
+                        <FormItem
+                            label="Permissions"
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('permissions', {
+                                rules: [{ required: true, message: 'Please input a permissions' }],
+                            })(
+                                <Select initialValue="1">
+                                    <Option value="1">Basic</Option>
+                                    <Option value="2">Premium</Option>
+                                </Select>
+                                )}
+                        </FormItem>
+                        <FormItem {...buttonItemLayout}>
+                            <Button type="primary" htmlType="submit">Submit</Button>
+                        </FormItem>
+                    </Form>
+                </Col>
+            </Row>
+        </Content>
+    </AdvancedLayout>
+)
+
 
 class Container extends React.Component {
     handleSubmit = (e) => {
@@ -41,99 +122,21 @@ class Container extends React.Component {
     }
 
     render = () => {
-        const { app, account, form: { getFieldDecorator } } = this.props
-
-        return (
-            <AdvancedLayout>
-                <Content style={{ 'padding': '10px', 'marginTop': '10px', 'background': '#fff' }}>
-                    Create App
-                    <br />
-                    <Row>
-                        <Col xs={24} sm={24} md={16} lg={16} xl={10}>
-                            <Form layout="horizontal" onSubmit={this.handleSubmit}>
-                                <FormItem
-                                    label="Name"
-                                    {...formItemLayout}
-                                >
-                                    {getFieldDecorator('name', {
-                                        rules: [{ required: true, message: 'Please input a name' }],
-                                    })(
-                                        <Input />
-                                    )}
-                                </FormItem>
-                                <FormItem
-                                    label="Version"
-                                    {...formItemLayout}
-                                >
-                                    {getFieldDecorator('version', {
-                                        rules: [{ required: true, message: 'Please input a version' }],
-                                    })(
-                                        <Input />
-                                    )}
-                                </FormItem>
-                                <FormItem
-                                    label="Category"
-                                    {...formItemLayout}
-                                >
-                                    {getFieldDecorator('category', {
-                                        rules: [{ required: true, message: 'Please input a category' }],
-                                    })(
-                                        <Input />
-                                    )}
-                                </FormItem>
-                                <FormItem
-                                    label="Files"
-                                    {...formItemLayout}
-                                >
-                                    {getFieldDecorator('files', {
-                                        rules: [{ required: true, message: 'Please input a files' }],
-                                    })(
-                                        <Input />
-                                    )}
-                                </FormItem>
-                                <FormItem
-                                    label="Checksum"
-                                    {...formItemLayout}
-                                >
-                                    {getFieldDecorator('checksum', {
-                                        rules: [{ required: true, message: 'Please input a checksum' }],
-                                    })(
-                                        <Input />
-                                    )}
-                                </FormItem>
-                                <FormItem
-                                    label="Permissions"
-                                    {...formItemLayout}
-                                >
-                                    {getFieldDecorator('permissions', {
-                                        rules: [{ required: true, message: 'Please input a permissions' }],
-                                    })(
-                                        <Select defaultValue="1">
-                                            <Option value="1">Basic</Option>
-                                            <Option value="2">Premium</Option>
-                                        </Select>
-                                    )}
-                                </FormItem>
-                                <FormItem {...buttonItemLayout}>
-                                    <Button type="primary" htmlType="submit">Submit</Button>
-                                </FormItem>
-                            </Form>
-                        </Col>
-                    </Row>
-                </Content>
-            </AdvancedLayout>
-        )
+        return <MainContent {...this.props} handleSubmit={this.handleSubmit.bind(this)} />
     }
 }
 
 Container.displayName = 'create-app/Container'
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state, ownProps) => ({
+    id: ownProps.match.params.id,
+    app: state.marketplace.selectedApp,
     account: state.account
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-    submitApp: MarketplaceActions.submitApp
+    getApp: (id) => dispatch(MarketplaceActions.getApp(id)),
+    submitApp: (name, version, category, files, checksum, permissions) => dispatch(MarketplaceActions.submitApp(name, version, category, files, checksum, permissions))
 }, dispatch)
 
 export default connect(
