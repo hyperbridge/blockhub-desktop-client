f4326f7a8bd238f269aad06497b619a504e3560d
Eric Muyser <eric@muyser.com>
2018-04-04 13:49:01 -0700

    App detail screen has a fallback app doesn't exist

--- src/containers/app-detail/index.js
+++ src/containers/app-detail/index.js
@@ -18,43 +18,72 @@ const handleRate = () => {
 
 }
 
-const Container = ({ app }) => (
+
+
+const MainContent = ({ app, account }) => (
     <AdvancedLayout>
         <Content style={{ padding: '10px', marginTop: '10px' }}>
-            <div>{app.title}</div>
-            <div>{app.publisher}</div>
-            <Rate onChange={handleRate} value={app.rating} />
-            {app.rating && <span className="ant-rate-text">{app.rating} stars</span>}
-            <br />
-            Downloads: {app.downloads}
-            <br />
-            {app.id}
-            <div>
-                tabs
-            </div>
-            <div>
-                friends who own this
-            </div>
-            <div>
-                achievements
-            </div>
-            <div>
-                news
-            </div>
-            <div>
-                Launch
-            </div>
+            {!app && (
+                <div>
+                    No app selected
+                        </div>
+            )}
+
+            {app && (
+                <div>
+                    <div>{app.name}</div>
+                    <div>{app.author.id}</div>
+                    <div>Type: {app.type}</div>
+                    <Rate onChange={handleRate} value={app.rating} />
+                    {app.rating && <span className="ant-rate-text">{app.rating} stars</span>}
+                    <br />
+                    Downloads: {app.downloads}
+                    <br />
+                    {app.id}
+                    <br />
+                    Tags: {app.authorTags.join(", ")}
+                    <div>
+                        tabs
+                            </div>
+                    <div>
+                        friends who own this
+                            </div>
+                    <div>
+                        achievements
+                            </div>
+                    <div>
+                        news
+                            </div>
+                    <div>
+                        Launch
+                            </div>
+                </div>
+            )}
         </Content>
     </AdvancedLayout>
 )
 
+
+class Container extends React.Component {
+    componentDidMount() {
+        this.props.getApp(this.props.id) //{ type: MarketplaceActions.APP_DETAIL_REQUEST, id: this.props.id }
+    }
+
+    render() {
+        return <MainContent {...this.props} />
+    }
+}
+
+
 Container.displayName = 'app-detail/Container'
 
 const mapStateToProps = (state, ownProps) => ({
-    app: MarketplaceActions.getApp(ownProps.match.params.id)
+    id: ownProps.match.params.id,
+    app: state.marketplace.selectedApp
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
+    getApp: (id) => dispatch(MarketplaceActions.getApp(id))
 }, dispatch)
 
 export default connect(
