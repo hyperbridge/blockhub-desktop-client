d3cd51cd8b93ea0d1429ab21975175292afa8bb6
Eric Muyser <eric@muyser.com>
2018-04-08 16:31:26 -0700

    Add redux-localstorage depends

--- src/store.js
+++ src/store.js
@@ -2,10 +2,13 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import createBrowserHistory from 'history/createBrowserHistory'
+import persistState from 'redux-localstorage'
 
 export const history = createBrowserHistory()
 export const initialState = {}
-export const enhancers = []
+export const enhancers = [
+    //persistState()
+]
 export const middleware = [
     thunk,
     routerMiddleware(history)
